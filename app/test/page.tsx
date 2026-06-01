0877265e54443e72ae50de114f3c733be3c690b6
Pakhomov Michael <pakhomovmichael@icloud.com>
2026-06-01 22:41:25 +0400

    Fix: Use Link instead of anchor tag

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export default function TestPage() {
   return (
     <div style={{ padding: '50px', textAlign: 'center' }}>
       <h1>Test Page Works!</h1>
       <p>If you see this, Next.js routing is working.</p>
-      <a href="/">Go to Home</a>
+      <Link href="/">Go to Home</Link>
     </div>
   )
 }
