bbe6122d8c4b5acdac1d4ee98720c9e40a32710c
Pakhomov Michael <pakhomovmichael@icloud.com>
2026-06-01 22:40:35 +0400

    Add test page for debugging

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, Next.js routing is working.</p>
+      <a href="/">Go to Home</a>
+    </div>
+  )
+}
