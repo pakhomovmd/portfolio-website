20d9ae84712b14a277d552abbd13b3f722504323
Pakhomov Michael <pakhomovmichael@icloud.com>
2026-06-03 22:34:23 +0400

    Try to deploy on Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     domains: ['github.com', 'avatars.githubusercontent.com'],
   },
 }
