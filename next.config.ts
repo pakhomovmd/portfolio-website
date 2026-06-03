2c02595752c2516d3d4cd7fc5b35bf3c93a236ee
Pakhomov Michael <pakhomovmichael@icloud.com>
2026-06-03 22:45:35 +0400

    Revert static export config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
   reactStrictMode: true,
   images: {
-    unoptimized: true,
     domains: ['github.com', 'avatars.githubusercontent.com'],
   },
 }
